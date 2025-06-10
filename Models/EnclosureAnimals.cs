6cd231454f470b578237d1806dc4da3d417bd2e5
Hannah Dunsmore <hannah.dunsmore@softwire.com>
2025-06-10 18:10:11 +0100

    removed animalsInEnclosure from GetAnimals / Zoo Controller return

--- Models/EnclosureAnimals.cs
+++
@@ -1,9 +0,0 @@
-namespace ZooManagement.Models;
-
-public class EnclosureAnimals
-{
-    public int EnclosureAnimalsId { get; set; }
-    public string? Enclosure { get; set; }
-
-    public List<Animal>? Animals { get; set; }
-}
--- Models/ZookeeperEnclosures.cs
+++
@@ -1,8 +0,0 @@
-namespace ZooManagement.Models;
-
-public class ZookeeperEnclosure
-{
-    public int ZookeeperId { get; set; }
-    public string? Name { get; set; }
-    public List<EnclosureAnimals>? EnclosureAllocations { get; set; }
-}
